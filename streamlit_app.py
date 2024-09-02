4e3f6a588838e15290d4c16a8f9cb11c02d49015
mulyankan-gurukul <ashishwebsite13@gmail.com>
2024-09-02 16:02:18 +0000

    pushed changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,36 @@
 import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def main():
+    api_key = st.text_input("Enter API key")
+    if not api_key:
+        with st.sidebar:
+            api_key = st.text_input("Enter the Open AI api key here")
+    
+    input_text = st.text_area("Enter the input for JIRA story creation")
+
+    if st.button("Create task"):
+        with st.spinner("Creating the task"):
+            llm = ChatOpenAI(model = "gpt-4o-mini", api_key = api_key, temperature = 0.7, max_tokens = 1024)
+
+            prompt = ChatPromptTemplate.from_messages([
+                ("system", """Act as a helpful expert scrum master and create task for the story defined, you will not use extremely technique terms, just be generic. The task should have the following structure.
+
+    Title : Title goes here
+
+    Description : Description goes here
+
+    Acceptance criteria : Acceptance goes here"""),
+                ("human", "{input_text}"),
+            ])
+
+            chain = prompt | llm
+            result = chain.invoke({
+                "input_text": input_text
+            })
+
+            st.write(result.content) 
+
+if __name__ == "__main__":
+    main()
